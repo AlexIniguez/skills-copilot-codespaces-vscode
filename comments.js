67a200ee9a2871408ce692e9f7b526fa6003a6dd
Alejandro Iñiguez <ilex_happy99@hotmail.com>
2023-11-29 17:05:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,70 @@
 //Create a web browser
-const browser = await puppeteer.launch({ headless: false });
-const page = await browser.newPage();
-//Go to the page
-await page.goto('https://www.instagram.com/p/CHH3lZ2lY5b/');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = mongoose.model('Comment');
 
-//Wait for the comments to load
-await page.waitForSelector('div.C4VMK span');
+//GET ALL COMMENTS
+router.get('/', function(req, res, next) {
+    Comment.find(function(err, comments){
+        if(err){ return next(err); }
 
-//Get the comments
-const comments = await page.$$eval('div.C4VMK span', links => {
-  //Make sure the comment is not empty
-  links = links.filter(link => link.innerText.trim());
-  //Extract the username and comment text
-  links = links.map(el => el.innerText.split('\n'));
-  //Return the results
-  return links;
+        res.json(comments);
+    });
 });
-//Output the comments to the console
-console.log(comments);
-//Close the browser
-await browser.close();
+
+//POST COMMENT
+router.post('/', function(req, res, next) {
+    var comment = new Comment(req.body);
+
+    comment.save(function(err, comment){
+        if(err){ return next(err); }
+
+        res.json(comment);
+    });
+});
+
+//PRELOAD COMMENT OBJECTS
+router.param('comment', function(req, res, next, id) {
+    var query = Comment.findById(id);
+
+    query.exec(function (err, comment){
+        if (err) { return next(err); }
+        if (!comment) { return next(new Error("can't find comment")); }
+
+        req.comment = comment;
+        return next();
+    });
+});
+
+//GET COMMENT
+router.get('/:comment', function(req, res) {
+    res.json(req.comment);
+});
+
+//DELETE COMMENT
+router.delete('/:comment', function(req, res) {
+    req.comment.remove();
+    res.sendStatus(200);
+});
+
+//UPVOTE COMMENT
+router.put('/:comment/upvote', function(req, res, next) {
+    req.comment.upvote(function(err, comment){
+        if (err) { return next(err); }
+
+        res.json(comment);
+    });
+});
+
+//DOWNVOTE COMMENT
+router.put('/:comment/downvote', function(req, res, next) {
+    req.comment.downvote(function(err, comment){
+        if (err) { return next(err); }
+
+        res.json(comment);
+    });
+});
+
+//EXPORT MODULE
+module.exports = router;
